a97dd620999f806af2bcdc4ff5fcce964cc0ac0e
Thomas <110282836+pyxelhaze@users.noreply.github.com>
2023-09-28 08:54:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+'use strict'
+
+const hamburger = () => {
+    let x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+// slideshow
+
+let slideIndex = 0;
+
+const carousel = () => {
+    let i;
+    let x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) { slideIndex = 1 }
+    x[slideIndex - 1].style.display = "block";
+    setTimeout(carousel, 4000);
+}
+carousel();
+
+// fullscreen img
+
+const images = document.querySelectorAll('.tanks_gallery, .tank_img, .plant_img');
+
+images.forEach(image => image.addEventListener('click', (e) => {
+    image.requestFullscreen();
+}))
+
+images.forEach(image => image.addEventListener('click', (e) => {
+    document.exitFullscreen();
+}))
+
+// modal
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+        closeModal(modal)
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    })
+})
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
